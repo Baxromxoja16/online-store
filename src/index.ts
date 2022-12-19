f8f7f12a32846cee8a743cbf80369a6ef56414bd
jahanbaev <sjahanbaev@gmail.com>
2022-12-19 13:51:50 +0500

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,21 @@ import useRoute from './scripts/route';
 window.onhashchange = useRoute.locationHashChanged;
 cart(); 
 
+const scripts = {
+    main: {
+        list: null, 
+        start(){
+            this.getProducts();
+        },
+        getProducts(){
+            if(this.list == null){
+                fetch('https://dummyjson.com/products?limit=100',{
+                    credentials: "include"
+                }).then(req => req.json()).then(res => {
+                    this.list = res;
+                })  
+            }
+        }
+    }
+}
+
