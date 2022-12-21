d5f3982e37eafd10e7d28af8eb180e6efd39c854
baxromxoja16 <aglamof@mail.ru>
2022-12-21 15:46:50 +0500

    feat: add popUp to cart page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import cart from './scripts/cart';
+import './scripts/cart';
 import './styles/index.sass';
 // import home from './scripts/home';
 import useRoute from './scripts/route';
 window.onhashchange = useRoute.locationHashChanged;
-cart(); 
+
 
 const scripts = {
     main: {
--- src/scripts/cart.ts
+++ src/scripts/cart.ts
@@ -1,7 +1,20 @@
+const app = <HTMLElement>document.querySelector('#app')!;
 const cart = {
     popUp(){
-        alert(document.querySelector(".buy")!.innerHTML);
+        app.addEventListener('click', (e:Event) => {
+            const popUpModal = <HTMLElement>app.children[3]
+            const HtmlElem = <HTMLElement>e.target
+            if (HtmlElem.className === 'buy') {
+                popUpModal.classList.add('active-modal')
+            }
+            if (HtmlElem.className === 'overlay') {
+                popUpModal.classList.remove('active-modal')
+            }
+        })
     }
 }
+cart.popUp()
 
-export default cart.popUp
+
+
+export default cart
