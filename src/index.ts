d76e4bcf28e1f4217e81d00817266c7a93d3bda9
jahanbaev <sjahanbaev@gmail.com>
2022-12-28 12:23:51 +0500

    feat: route start script

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,44 @@
 import cart from './scripts/cart';
 import './styles/index.sass';
+import './styles/galery.sass'
 // import home from './scripts/home';
 import useRoute from './scripts/route';
-window.onhashchange = useRoute.locationHashChanged;
-window.addEventListener('hashchange', () => {
-    cart
-})
+
 
 const scripts = {
     main: {
-        list: null, 
+        list: {"products":[{"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/1/1.jpg","https://i.dummyjson.com/data/products/1/2.jpg","https://i.dummyjson.com/data/products/1/3.jpg","https://i.dummyjson.com/data/products/1/4.jpg","https://i.dummyjson.com/data/products/1/thumbnail.jpg"]}],"total":100,"skip":0,"limit":1}, 
         start(){
+            console.log("started")
             this.getProducts();
         },
         getProducts(){
-            if(this.list == null){
-                fetch('https://dummyjson.com/products?limit=100',{
-                    credentials: "include"
+                fetch('https://dummyjson.com/products?limit=30',{
                 }).then(req => req.json()).then(res => {
                     this.list = res;
                     this.setList()
-                })  
-            }
+                })   
         },
         setList(){
-            document.querySelector("#list")
+            this.list.products.forEach(e => {
+                let card = document.createElement('div');
+                card.classList.add('card');
+                card.innerHTML = ` <img src="${e.images[0]}" alt="">
+                <p class="title">${e.title}</p>
+                <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum!</p>`
+                document.querySelector(".grid")!.appendChild(card);
+            });
+            
+            
         }
     }
 }
+
+useRoute.locationHashChanged(scripts);
+window.addEventListener('hashchange', () => {
+
+      // cart
+})
 
 // types
 interface ProductItem {
--- src/scripts/route.ts
+++ src/scripts/route.ts
@@ -5,27 +5,31 @@ interface jscode {
     page: object
 }
 const useRoute = {
-    route(to: string): void{
+    route(to: string, script: any): void{
+        let url : string = (to === "pages//.html" || to === "pages/.html" )?"pages/main.html":to;
         const app = document.querySelector("#app");
         const links = document.querySelectorAll('[route="link"]');
-         fetch((to === "pages//.html" || to === "pages/.html" )?"pages/main.html":to,{
+         fetch(url,{
             credentials: "include"
         }).then(req => req.text()).then(res => {
             app!.innerHTML = res;
+
+            script[url.split('/')[1].split('.')[0]].start()
+            console.log(script[url.split('/')[1].split('.')[0]])
             links.forEach(e => {
                 e.classList.remove("route-active")
             })
-            if( to != ".html" ) {  
+            if( to != ".html" ) {
                 document.querySelector(`[href="#${to.replace(".html", "").replace("pages/","")}"]`)?.classList.add("route-active")
             }
         })
     },
-    locationHashChanged(): void {
-        useRoute.route("pages/"+location.hash.toString().replace("#", "") + ".html" );
+    locationHashChanged(script : object): void {
+        useRoute.route("pages/"+location.hash.toString().replace("#", "") + ".html", script );
     }
 }
 
-useRoute.locationHashChanged()
+
 
 
 
